43e0d24ec63925f549c4cb2657bccd756e0cac50
abradle <anthony.richard.bradley@gmail.com>
2017-03-22 15:37:23 +0000

    Added test data

--- convert_sdf.py
+++ convert_sdf.py
@@ -5,11 +5,12 @@ from pubchempy import *
 mols = Chem.SDMolSupplier(sys.argv[1])
 
 for cmpd in mols:
-  print Chem.MolToSmiles(cmpd,isomericSmiles=True)
   cmpds = get_compounds(Chem.MolToSmiles(cmpd,isomericSmiles=True), 'smiles')
   for c in cmpds:
+    cas_id = None
     if c.canonical_smiles:
-      print(c.synonyms)
       for syn in c.synonyms:
         if re.search('CAS-\d{2,7}-\d\d-\d',syn):
-          print(syn)
+          cas_id = syn
+    if cas_id:
+      print(c.canonical_smiles,syn)
