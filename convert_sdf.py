8a01c9d3a6d86a4dbdc81e522e7ca689b521abdc
abradle <anthony.richard.bradley@gmail.com>
2017-03-28 09:36:49 +0100

    Updated convert SDF

--- convert_sdf.py
+++ convert_sdf.py
@@ -3,7 +3,6 @@ from rdkit import Chem
 from pubchempy import *
 
 mols = Chem.SDMolSupplier(sys.argv[1])
-
 for cmpd in mols:
   cmpds = get_compounds(Chem.MolToSmiles(cmpd,isomericSmiles=True), 'smiles')
   for c in cmpds:
@@ -12,5 +11,7 @@ for cmpd in mols:
       for syn in c.synonyms:
         if re.search('CAS-\d{2,7}-\d\d-\d',syn):
           cas_id = syn
+          break
     if cas_id:
       print(c.canonical_smiles,syn)
+    break
